4e278e48b9deb9ec69966783a0dd415bbb336209
Variable Username <var-username@users.noreply.github.com>
2017-09-27 17:26:23 -0500

    Uploaded from local storage

---
+++ config.h
@@ -0,0 +1,19 @@
+/*
+ * config.h
+ *
+ *  Created on: Aug 3, 2017
+ *      Author: variable
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_TWEAK 0
+#define VERSION_SUFFIX "dev"
+#define VERSION "v0.1.0-dev"
+#define AUTHOR "Variable Username"
+#define COPYRIGHT "(C) Variable Username 2017"
+
+#endif /* CONFIG_H_ */
